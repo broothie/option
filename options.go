03028f5f71a7a9bf7180740b105d7ff2e050f3e6
Andrew Booth <adbooth8@gmail.com>
2025-06-23 21:05:43 -0700

    fix formatting for nil option check

--- options.go
+++ options.go
@@ -23,6 +23,7 @@ func (o Options[T]) Apply(t T) (T, error) {
 		if option == nil {
 			return t, fmt.Errorf("option %d is nil", i)
 		}
+
 		var err error
 		if t, err = option.Apply(t); err != nil {
 			return t, fmt.Errorf("failed to apply option %d: %w", i, err)
